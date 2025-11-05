d42f72a74d0bf05747cf3d17e7de77d98465a726
AdrenoEz <redarmedcommando@gmail.com>
2025-11-05 22:22:12 +0530

    Create methodscope.java

---
+++ methodscope.java
@@ -0,0 +1,26 @@
+public class Main {
+    public static void main(String[] args) {
+        String adreno = "Iron man";
+        greet(adreno);
+    }
+
+    static void greet(String adreno) {
+        System.out.println(adreno);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int marks = 88;
+        System.out.println(marks);
+        random(marks);
+    }
+
+    static void random(int marks) {
+        int num = 67;
+        System.out.println(num);
+        System.out.println(marks);
+    }
+}
