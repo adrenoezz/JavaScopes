101d028be216cb6c841103b9d642b6915c312ddd
AdrenoEz <redarmedcommando@gmail.com>
2025-11-05 22:23:16 +0530

    Create loopblock.java
    loop scope
    block scope

---
+++ loopblock.java
@@ -0,0 +1,37 @@
+public class Main {
+    static void main(String[] args) {
+        int a = 2;
+        int b = 4;
+
+        {
+            a = 100;
+            System.out.println(a);
+            int c = 99;
+        }
+    }
+
+    static void random(int marks) {
+        int num = 77;
+        System.out.println(num);
+        System.out.println(marks);
+    }
+}
+
+public class Main {
+    static void main(String[] args) {
+        int a = 109999;
+
+        for (int i = 0; i < 4; i++) {
+            System.out.println(i);
+            int num = 200;
+        }
+
+        System.out.println();
+    }
+
+    static void random(int marks) {
+        int num = 77;
+        System.out.println(num);
+        System.out.println(marks);
+    }
+}
